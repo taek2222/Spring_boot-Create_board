88eaef4746fe8f48546348f2fdff44f44ac8dba2
taek2222 <oot309@daum.net>
2023-12-23 19:25:35 +0900

    [Practice] 연습 프로젝트

--- board/src/main/java/com/example/board/controller/ArticleController.java
+++ board/src/main/java/com/example/board/controller/ArticleController.java
@@ -2,12 +2,15 @@ package com.example.board.controller;
 
 import com.example.board.dto.ArticleForm;
 import com.example.board.entity.Article;
+import com.example.board.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    @Autowired
     private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
@@ -20,8 +23,10 @@ public class ArticleController {
         System.out.println(form.toString());
         // 1. DTO를 엔터리로 변환
         Article article = form.toEntity();
+        System.out.println(article.toString());
         // 2. 리파지터리로 엔터티를 DB에 저장
         Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
         return "";
     }
 }
