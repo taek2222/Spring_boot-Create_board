c26d0da795848abffcc42aa0a1fbfd99c1ac9ac9
taek2222 <oot309@daum.net>
2024-01-08 00:34:18 +0900

    [Practice] 연습 프로젝트

--- board/src/main/java/com/example/board/entity/Article.java
+++ board/src/main/java/com/example/board/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.board.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Article {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String title;
