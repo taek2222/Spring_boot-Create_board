4aeb1e586cc389fb3e4461a550f2b5fd3250cc8c
taek2222 <oot309@daum.net>
2024-01-10 18:12:20 +0900

    [Practice] 연습 프로젝트

---
+++ board/src/main/java/com/example/board/api/ArticleApiController.java
@@ -0,0 +1,56 @@
+package com.example.board.api;
+
+import com.example.board.dto.ArticleForm;
+import com.example.board.entity.Article;
+import com.example.board.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class ArticleApiController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    //GET
+    @GetMapping("/api/articles")
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    //POST
+    @PostMapping("/api/articles")
+    public Article create(@RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        return articleRepository.save(article);
+    }
+
+    //PATCH
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+
+        Article target = articleRepository.findById(id).orElse(null);
+
+        if (target == null || id != article.getId()) {
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        target.patch(article);
+        Article updated = articleRepository.save(article);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+    //DELETE
+}
--- board/src/main/java/com/example/board/entity/Article.java
+++ board/src/main/java/com/example/board/entity/Article.java
@@ -33,4 +33,11 @@ public class Article {
                 ", content='" + content + '\'' +
                 '}';
     }
+
+    public void patch(Article article) {
+        if(article.title != null)
+            this.title = article.title;
+        if(article.content != null)
+            this.content = article.content;
+    }
 }
